0e4264444fb31a727010cb0b3738c3054c7b4f89
apenkomix <106159946+apenkomix@users.noreply.github.com>
2022-08-18 15:42:50 +0300

    Delete java-core directory

--- java-core/src/main/java/homework_15/Task_2/Student.java
+++
@@ -1,4 +0,0 @@
-package homework_15.Task_2;
-
-public record Student(String id, String firstname, String lastname, String groupNumber, String favoritSubject) {
-}
--- java-core/src/main/java/homework_20/task_1/Stock.java
+++
@@ -1,4 +0,0 @@
-package homework_20.task_1;
-
-public record Stock(Long id, String ticker, String company_name,Long profit_year, String location,String updated_data_time ) {
-}
