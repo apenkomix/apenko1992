bed60e24efcdab7d7b07e2d25d20b6c55b8a59b7
Mary Stankevich <marrycitty@mail.ru>
2022-09-08 17:51:21 +0200

    Update HouseDto.java

--- java-spring/src/main/java/org/example/javaspring/homework_24/dto/HouseDto.java
+++ java-spring/src/main/java/org/example/javaspring/homework_24/dto/HouseDto.java
@@ -2,5 +2,6 @@ package org.example.javaspring.homework_24.dto;
 
 import org.example.javaspring.homework_24.entity.HouseType;
 
+//код не отформатирован
 public record HouseDto(Long id, HouseType houseType, Integer floor, Integer entrance,AddressDto address) {
 }
